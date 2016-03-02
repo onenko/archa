345b835a347952ebe5596c8685d9b62cbaf53e45
onenko <alex.nenko@gmail.com>
2016-03-02 15:27:55 +0200

    updated

--- aa-wea.js
+++ aa-wea.js
@@ -2,18 +2,20 @@
  aa-wea.js
  Assumed aa.js, aa-itm.js included
 */
-aa.WEA_BOW = 0x0001;
-aa.WEA_SHL = 0x0002;
-aa.WEA_SW1 = 0x0004;
-aa.WEA_SW2 = 0x0008;
-aa.WEA_SCP = 0x0010;
-aa.WEA_SP1 = 0x0020;
-aa.WEA_SP2 = 0x0040;
-aa.WEA_STF = 0x0080;
-aa.WEA_AX1 = 0x0100;
-aa.WEA_AX2 = 0x0200;
-aa.WEA_KAT = 0x0400;
-aa.WEA_NOD = 0x0800;
+aa.wea = {
+  WEA_BOW: 0x0001,
+  WEA_SHL: 0x0002,
+  WEA_SW1: 0x0004,
+  WEA_SW2: 0x0008,
+  WEA_SCP: 0x0010,
+  WEA_SP1: 0x0020,
+  WEA_SP2: 0x0040,
+  WEA_STF: 0x0080,
+  WEA_AX1: 0x0100,
+  WEA_AX2: 0x0200,
+  WEA_KAT: 0x0400,
+  WEA_NOD: 0x0800
+};
 
 aa.items.ItemWName1.props = { thProp: 'thProp1', thGrade: AA_ITEM_BASIC, thVal1: 54, thLink: './th/reports/1455_DATA_LOAD_231054944/1455_DATA_LOAD_231054944-PIN-PRODUCT.PR_XREF.csv.gz' }
 aa.items.ItemWName2.props = { thProp: 'thProp2', thGrade: AA_ITEM_GREAT, thVal1: 2, thLink: './th/reports/1455_DATA_LOAD_231054944/1456_DATA_LOAD_231054944-PIN-PRODUCT.PR_XREF.csv.gz' }
